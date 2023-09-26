cc740d4bf34c555e1d0dfdb149f501df8d5e6dbb
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-09-26 09:11:43 +0530

    First Shell script

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,17 @@
 #To print there are multiple commands
 
 echo hello world
+
+# While printing we may need colours to get users attention
+# syntax : echo -e[COLmMESSAGE\e[0m"
+# -e to enable colours
+#\e[COLm to enable certain colours
+#\e[0m to disable colour
+# COL stands for colours, possible colours are red(31), green(32), yellow(33), blue(34), magenta(35), cyan(36)
+
+echo -e "\e[31mHello in Red Color\e[0m"
+echo -e "\e[32mHello in Green Color\e[0m"
+echo -e "\e[33mHello in Yellow Color\e[0m"
+echo -e "\e[34mHello in Blue Colour\e[0m"
+echo -e "\e[35mHello in Magenta Colour\e[0m"
+echo -e "\e[36mHello in Cyan Colour\e[0m"
